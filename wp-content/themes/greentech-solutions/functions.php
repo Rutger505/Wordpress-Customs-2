d9aed22e2a7b982f4379c7375b1b17a91eb8fe4d
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-19 14:30:05 +0100

    Remove theme from template repository

--- wp-content/themes/greentech-solutions/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Greentech Solutions Theme Functions
- */
-
-// Add theme support for featured images
-add_theme_support( 'post-thumbnails' );
--- wp-content/themes/greentech-solutions/scripts.php
+++
@@ -1,7 +0,0 @@
-<!-- Scripts -->
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/jquery.min.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/browser.min.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/breakpoints.min.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/util.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/main.js"></script>
-
