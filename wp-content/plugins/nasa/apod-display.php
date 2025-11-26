b4ba764040350fd13f5c0ba1046a0c99f386b0fb
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-26 14:21:37 +0100

    Create seperate template files

---
+++ wp-content/plugins/nasa/apod-display.php
@@ -0,0 +1,61 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function nasa_apod_display($data, $atts) {
+    if (isset($data['error'])) {
+        return '<div class="nasa-apod-error">Error: ' . esc_html($data['error']) . '</div>';
+    }
+
+    ob_start();
+    ?>
+    <div class="nasa-apod-container">
+        <?php if ($atts['show_title'] === 'yes' && !empty($data['title'])): ?>
+            <h2 class="nasa-apod-title"><?php echo esc_html($data['title']); ?></h2>
+        <?php endif; ?>
+
+        <?php if ($atts['show_date'] === 'yes' && !empty($data['date'])): ?>
+            <p class="nasa-apod-date">
+                <strong>Date:</strong> <?php echo esc_html(date('F j, Y', strtotime($data['date']))); ?>
+            </p>
+        <?php endif; ?>
+
+        <div class="nasa-apod-media" style="max-width: <?php echo esc_attr($atts['width']); ?>;">
+            <?php if ($data['media_type'] === 'image'): ?>
+                <img src="<?php echo esc_url($data['url']); ?>"
+                     alt="<?php echo esc_attr($data['title']); ?>"
+                     class="nasa-apod-image">
+                <?php if (!empty($data['hdurl'])): ?>
+                    <p class="nasa-apod-hd-link">
+                        <a href="<?php echo esc_url($data['hdurl']); ?>" target="_blank">
+                            View HD Version
+                        </a>
+                    </p>
+                <?php endif; ?>
+            <?php elseif ($data['media_type'] === 'video'): ?>
+                <div class="nasa-apod-video">
+                    <iframe src="<?php echo esc_url($data['url']); ?>"
+                            frameborder="0"
+                            allowfullscreen
+                            class="nasa-apod-iframe"></iframe>
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <?php if ($atts['show_description'] === 'yes' && !empty($data['explanation'])): ?>
+            <div class="nasa-apod-explanation">
+                <h3>Explanation</h3>
+                <p><?php echo nl2br(esc_html($data['explanation'])); ?></p>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($atts['show_copyright'] === 'yes' && !empty($data['copyright'])): ?>
+            <p class="nasa-apod-copyright">
+                <strong>Copyright:</strong> <?php echo esc_html($data['copyright']); ?>
+            </p>
+        <?php endif; ?>
+    </div>
+    <?php
+    return ob_get_clean();
+}
---
+++ wp-content/plugins/nasa/settings.php
@@ -0,0 +1,62 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function nasa_apod_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>NASA APOD Settings</h1>
+
+        <form method="post" action="options.php">
+            <?php settings_fields('nasa_apod_settings'); ?>
+            <?php do_settings_sections('nasa_apod_settings'); ?>
+
+            <table class="form-table">
+                <tr>
+                    <th scope="row">
+                        <label for="nasa_apod_api_key">NASA API Key</label>
+                    </th>
+                    <td>
+                        <input type="text"
+                               id="nasa_apod_api_key"
+                               name="nasa_apod_api_key"
+                               value="<?php echo esc_attr(get_option('nasa_apod_api_key', 'DEMO_KEY')); ?>"
+                               class="regular-text">
+                        <p class="description">
+                            Get your free API key at <a href="https://api.nasa.gov" target="_blank">https://api.nasa.gov</a>
+                            <br>Using DEMO_KEY is limited to 30 requests per hour per IP address.
+                        </p>
+                    </td>
+                </tr>
+            </table>
+
+            <?php submit_button(); ?>
+        </form>
+
+        <hr>
+
+        <h2>How to Use</h2>
+        <p>Use the shortcode <code>[nasa_apod]</code> in any post or page to display today's Astronomy Picture of the Day.</p>
+
+        <h3>Shortcode Parameters:</h3>
+        <ul>
+            <li><code>[nasa_apod]</code> - Display today's APOD</li>
+            <li><code>[nasa_apod date="2024-01-01"]</code> - Display APOD from a specific date</li>
+            <li><code>[nasa_apod width="800px"]</code> - Set custom width</li>
+            <li><code>[nasa_apod show_title="no"]</code> - Hide title</li>
+            <li><code>[nasa_apod show_description="no"]</code> - Hide description</li>
+            <li><code>[nasa_apod show_date="no"]</code> - Hide date</li>
+            <li><code>[nasa_apod show_copyright="no"]</code> - Hide copyright</li>
+        </ul>
+
+        <h3>Preview:</h3>
+        <div style="max-width: 800px;">
+            <?php
+            $plugin = new NASA_APOD_Plugin();
+            echo $plugin->display_apod(array());
+            ?>
+        </div>
+    </div>
+    <?php
+}
